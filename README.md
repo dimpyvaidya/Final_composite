d87231d62283e56e57ac08c526a534bcbc99742f
Dimpy <dimpy13parekh@gmail.com>
2019-12-10 17:08:09 -0500

    functional working

---
+++ README.md
@@ -0,0 +1,4 @@
+References:
+1. My MscIT program project for various products (clothes,watches,bags,etc.)
+2. https://www.w3schools.com/
+3. https://www.daniweb.com/
---
+++ js/main.js
@@ -0,0 +1,625 @@
+var products = [{
+        id: 1111,
+        name: 'Nice Watch',
+        price: '199',
+        rating: '3',
+        description: "Nice Watch",
+        category: 'watches',
+        type: 'chain',
+        color: 'black',
+        size: 'small',
+        image: 'images/wap1.jpg'
+    },
+    {
+        id: 1112,
+        name: 'Red Watch',
+        price: '150',
+        rating: '2',
+        description: "gold watch",
+        category: 'watches',
+        type: 'sports',
+        color: 'all',
+        size: 'small',
+        image: 'images/wap2.jpg'
+    },
+    {
+        id: 1113,
+        name: 'Dobo Watch',
+        price: '200',
+        rating: '4',
+        description: "gold watch",
+        category: 'watches',
+        type: 'casual',
+        color: 'gold',
+        size: 'small',
+        image: 'images/wap3.jpg'
+    },
+    {
+        id: 1114,
+        name: 'Hodo Watch',
+        price: '250',
+        rating: '3',
+        description: "gold watch",
+        category: 'watches',
+        type: 'leather',
+        color: 'brown',
+        size: 'extra-large',
+        image: 'images/wap4.jpg'
+    },
+    {
+        id: 1115,
+        name: 'P-Shock Watch',
+        price: '280',
+        rating: '4',
+        description: "gold watch",
+        category: 'watches',
+        type: 'sports',
+        color: 'black',
+        size: 'large',
+        image: 'images/wap5.jpg'
+    },
+    {
+        id: 1116,
+        name: 'xxxx Watch',
+        price: '310',
+        rating: '5',
+        description: "gold watch",
+        category: 'watches',
+        type: 'casual',
+        color: 'blue',
+        size: 'large',
+        image: 'images/wap6.jpg'
+    },
+    {
+        id: 1117,
+        name: 'Max Watch',
+        price: '350',
+        rating: '5',
+        description: "gold watch",
+        category: 'watches',
+        type: 'leather',
+        color: 'brown',
+        size: 'medium',
+        image: 'images/wap7.jpg'
+    },
+    {
+        id: 1118,
+        name: 'Ruhu Watch',
+        price: '400',
+        rating: '1',
+        description: "gold watch",
+        category: 'watches',
+        type: 'chain',
+        color: 'black',
+        size: 'medium',
+        image: 'images/wap8.jpg'
+    },
+    {
+        id: 1119,
+        name: 'huku Watch',
+        price: '500',
+        rating: '4',
+        description: "gold watch",
+        category: 'watches',
+        type: 'sports',
+        color: 'black',
+        size: 'medium',
+        image: 'images/wap9.jpg'
+    },
+    {
+        id: 1120,
+        name: 'Nice Watch',
+        price: '99',
+        rating: '3',
+        description: "Nice Watch",
+        category: 'watches',
+        type: 'chain',
+        color: 'gold',
+        size: 'small',
+        image: 'images/wap10.jpg'
+    },
+    {
+        id: 1121,
+        name: 'Red Watch',
+        price: '195',
+        rating: '4',
+        description: "gold watch",
+        category: 'watches',
+        type: 'leather',
+        color: 'brown',
+        size: 'small',
+        image: 'images/wap11.jpg'
+    },
+    {
+        id: 1122,
+        name: 'Titan Watch',
+        price: '220',
+        rating: '4',
+        description: "gold watch",
+        category: 'watches',
+        type: 'leather',
+        color: 'brown',
+        size: 'small',
+        image: 'images/wap12.jpg'
+    },
+    {
+        id: 1123,
+        name: 'Hodo Watch',
+        price: '260',
+        rating: '3',
+        description: "gold watch",
+        category: 'watches',
+        type: 'leather',
+        color: 'blue',
+        size: 'extra-large',
+        image: 'images/wap13.jpg'
+    },
+    {
+        id: 1124,
+        name: 'S-Shock Watch',
+        price: '420',
+        rating: '4',
+        description: "gold watch",
+        category: 'watches',
+        type: 'chain',
+        color: 'gold',
+        size: 'large',
+        image: 'images/wap14.jpg'
+    },
+    {
+        id: 1125,
+        name: 'quizt Watch',
+        price: '310',
+        rating: '5',
+        description: "gold watch",
+        category: 'watches',
+        type: 'chain',
+        color: 'blue',
+        size: 'large',
+        image: 'images/wap15.jpg'
+    },
+    {
+        id: 1126,
+        name: 'Rado Watch',
+        price: '350',
+        rating: '5',
+        description: "gold watch",
+        category: 'watches',
+        type: 'casual',
+        color: 'all',
+        size: 'medium',
+        image: 'images/wap16.jpg'
+    },
+    {
+        id: 1127,
+        name: 'Ruhu Watch',
+        price: '400',
+        rating: '1',
+        description: "gold watch",
+        category: 'watches',
+        type: 'chain',
+        color: 'blue',
+        size: 'medium',
+        image: 'images/wap17.jpg'
+    },
+    {
+        id: 1128,
+        name: 'huku Watch',
+        price: '500',
+        rating: '4',
+        description: "gold watch",
+        category: 'watches',
+        type: 'casual',
+        color: 'all',
+        size: 'medium',
+        image: 'images/wap18.jpg'
+    },
+    {
+        id: 1129,
+        name: 'Timex Watch',
+        price: '400',
+        rating: '1',
+        description: "gold watch",
+        category: 'watches',
+        type: 'leather',
+        color: 'black',
+        size: 'medium',
+        image: 'images/wap19.jpg'
+    },
+    {
+        id: 1130,
+        name: 'G-shok Watch',
+        price: '530',
+        rating: '4',
+        description: "gold watch",
+        category: 'watches',
+        type: 'casual',
+        color: 'blue',
+        size: 'medium',
+        image: 'images/wap20.jpg'
+    },
+    {
+        id: 1131,
+        name: 'Handbag',
+        price: '99',
+        rating: '2',
+        description: "gold watch",
+        category: 'handbag',
+        type: 'leather',
+        color: 'red',
+        size: 'small',
+        image: 'images/hp1.jpg'
+    },
+    {
+        id: 1132,
+        name: 'Handbag',
+        price: '99',
+        rating: '3',
+        description: "gold watch",
+        category: 'handbag',
+        type: 'leather',
+        color: 'red',
+        size: 'small',
+        image: 'images/hp2.jpg'
+    },
+    {
+        id: 1133,
+        name: 'Handbag',
+        price: '99',
+        rating: '3',
+        description: "gold watch",
+        category: 'handbag',
+        type: 'leather',
+        color: 'red',
+        size: 'small',
+        image: 'images/hp3.jpg'
+    },
+    {
+        id: 1134,
+        name: 'Handbag',
+        price: '99',
+        rating: '3',
+        description: "gold watch",
+        category: 'handbag',
+        type: 'leather',
+        color: 'red',
+        size: 'small',
+        image: 'images/hp4.jpg'
+    },
+    {
+        id: 1135,
+        name: 'Handbag',
+        price: '99',
+        rating: '3',
+        description: "gold watch",
+        category: 'handbag',
+        type: 'leather',
+        color: 'red',
+        size: 'small',
+        image: 'images/hp5.jpg'
+    },
+    {
+        id: 1136,
+        name: 'Handbag',
+        price: '99',
+        rating: '3',
+        description: "gold watch",
+        category: 'handbag',
+        type: 'leather',
+        color: 'red',
+        size: 'small',
+        image: 'images/hp6.jpg'
+    },
+    {
+        id: 1137,
+        name: 'Handbag',
+        price: '80',
+        rating: '3',
+        description: "gold watch",
+        category: 'handbag',
+        type: 'leather',
+        color: 'red',
+        size: 'small',
+        image: 'images/hp7.jpg'
+    },
+    {
+        id: 1138,
+        name: 'Handbag',
+        price: '99',
+        rating: '3',
+        description: "gold watch",
+        category: 'handbag',
+        type: 'leather',
+        color: 'red',
+        size: 'small',
+        image: 'images/hp8.jpg'
+    },
+    {
+        id: 1139,
+        name: 'Handbag',
+        price: '99',
+        rating: '3',
+        description: "gold watch",
+        category: 'handbag',
+        type: 'leather',
+        color: 'red',
+        size: 'small',
+        image: 'images/hp9.jpg'
+    },
+];
+var records_per_page = 9;
+var main_product_list = products;
+var current_page = 1;
+var pr_cate = 'watches';
+var watchedArray = products.filter(function(itm) {
+    return itm.category == 'watches';
+});
+var filterOptions = [];
+var str = getProductList(watchedArray);
+
+document.getElementById('products_list').innerHTML = str;
+
+var classname = document.getElementsByClassName("filter");
+var arr_filter = document.getElementsByClassName("attr-filter");
+var price_filter = document.getElementsByClassName("price-filter");
+var star_filter = document.getElementsByClassName("star-filter");
+var sort_filter = document.getElementsByClassName("sort-filter");
+
+var myFunction = function() {
+    var attribute = this.getAttribute("data-type");
+
+    if (attribute == 'handbag') {
+        pr_cate = 'handbag';
+        var HandbagArray = products.filter(function(itm) {
+            return itm.category == 'handbag';
+        });
+
+        var str = getProductList(HandbagArray);
+        document.getElementById('products_list').innerHTML = str;
+    } else {
+        pr_cate = 'wacthes';
+        var watchedArray = products.filter(function(itm) {
+            return itm.category == 'watches';
+        });
+
+        var str = getProductList(watchedArray);
+        document.getElementById('products_list').innerHTML = str;
+    }
+};
+var filtering = function() {
+    var type = this.getAttribute("data-type");
+    var val = this.getAttribute("data-val");
+    const found = filterOptions.some(el => el[type] === val);
+    if (!found) {
+        filterOptions.push({
+            [type]: val
+        });
+    }
+    let condition = '';
+    var filter_array = [];
+    for (var product in products) {
+        for (var filter in filterOptions) {
+            for (var key in filterOptions[filter]) {
+                if (products[product][key] == filterOptions[filter][key] && products[product].category == pr_cate) {
+                    filter_array.push(products[product]);
+                }
+            }
+        }
+    }
+    var element = document.getElementById("clr_btn");
+    element.classList.remove("hidden");
+    var str = getProductList(filter_array);
+    document.getElementById('products_list').innerHTML = str;
+}
+var starFilter = function() {
+    var type = this.getAttribute("data-type");
+    var val = this.getAttribute("data-val");
+    filterOptions = filterOptions.filter(function(item, index, obj) {
+        return !("rating" in item);
+    });
+    filterOptions.push({
+        [type]: val
+    });
+    let condition = '';
+    var filter_array = [];
+    for (var product in products) {
+        for (var filter in filterOptions) {
+            for (var key in filterOptions[filter]) {
+                if (products[product][key] == filterOptions[filter][key] && products[product].category == pr_cate) {
+                    filter_array.push(products[product]);
+                }
+            }
+        }
+    }
+    var element = document.getElementById("clr_btn");
+    element.classList.remove("hidden");
+    var str = getProductList(filter_array);
+    document.getElementById('products_list').innerHTML = str;
+}
+var priceFilter = function() {
+    var value = this.getAttribute("data-val");
+    var HandbagArray = products.filter(function(itm) {
+        return parseInt(itm.price) <= parseInt(value) && itm.category == pr_cate;
+    });
+    var str = getProductList(HandbagArray);
+    document.getElementById('products_list').innerHTML = str;
+};
+
+var sortFilter = function() {
+    var value = this.getAttribute("data-type");
+    var filter = products.filter(function(itm) {
+        return itm.category == pr_cate;
+    });
+    if (value == 'name')
+        var result = sortByKey(filter, value, this.getAttribute("data-val"));
+    else {
+        if (this.getAttribute("data-val") == 'asc') {
+            var result = filter.sort(function(a, b) {
+                var x = parseInt(a.price);
+                var y = parseInt(b.price);
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+            });
+        } else {
+            var result = filter.sort(function(a, b) {
+                var x = parseInt(a.price);
+                var y = parseInt(b.price);
+                return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+            });
+        }
+    }
+    var str = getProductList(result);
+    document.getElementById('products_list').innerHTML = str;
+}
+for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', myFunction, false);
+}
+for (var i = 0; i < arr_filter.length; i++) {
+    arr_filter[i].addEventListener('click', filtering, false);
+}
+for (var i = 0; i < price_filter.length; i++) {
+    price_filter[i].addEventListener('click', priceFilter, false);
+}
+for (var i = 0; i < star_filter.length; i++) {
+    star_filter[i].addEventListener('click', starFilter, false);
+}
+for (var i = 0; i < sort_filter.length; i++) {
+    sort_filter[i].addEventListener('click', sortFilter, false);
+}
+
+function getProductList(plist, page = 1) {
+    main_product_list = plist;
+    var btn_next = document.getElementById("btn_next");
+    var btn_prev = document.getElementById("btn_prev");
+    var listing_table = document.getElementById("products_list");
+    var page_span = document.getElementById("page");
+
+    if (page < 1) page = 1;
+    if (page > numPages(plist)) page = numPages(plist);
+
+    listing_table.innerHTML = "";
+
+    var str = '';
+
+    for (var i = (page - 1) * records_per_page; i < (page * records_per_page) && i < plist.length; i++) {
+        str = str + '<div class="filter_portion product_block">';
+        str = str + '<a href="javascript:;" onClick="details(' + plist[i].id + ')"><div class="product-img">';
+        str = str + '<img src="' + plist[i].image + '" alt="" />';
+        str = str + '</div></a>';
+        var gold_length = parseInt(plist[i].rating);
+        var gray_length = 5 - parseInt(plist[i].rating);
+        for (var k = 0; k < gold_length; k++) {
+            str = str + "<i><img src='images/star.png' class='starimg' aria-hidden='true'></i>";
+        }
+        for (var k = 0; k < gray_length; k++) {
+            str = str + "<i><img src='images/star-gray.png' class='starimg' aria-hidden='true'></i>";
+        }
+        str = str + '<h4>' + plist[i].name + '</h4>';
+        str = str + '<h5>' + '$' + plist[i].price + '</h5></div>';
+    }
+
+    page_span.innerHTML = page + " of " + numPages(plist);
+
+    if (page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page == numPages(plist)) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
+    return str;
+}
+
+//clear search
+function clearAllFilters() {
+    window.location.reload();
+}
+
+//searching product
+function searchProduct() {
+    var value = (document.getElementById("product_name").value).toLowerCase();
+    var nameArray = products.filter(function(itm) {
+        var name = itm.name.toLowerCase();
+        return name.indexOf(value) >= 0 && itm.category == pr_cate;
+    });
+    var str = getProductList(nameArray);
+    document.getElementById('products_list').innerHTML = str;
+}
+
+//ascending-descending
+function sortByKey(array, key, order) {
+    if (order == 'asc') {
+        return array.sort(function(a, b) {
+            var x = a[key];
+            var y = b[key];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+    } else {
+        return array.sort(function(a, b) {
+            var x = a[key];
+            var y = b[key];
+            return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+        });
+    }
+}
+
+//particular product 
+function details(id) {
+    product = products.find(x => x.id === id);
+    let image = '<img  src="' + product.image + '" data-imagezoom="true" alt="">';
+    let name = product.name;
+    let description = product.description;
+    var gold_length = parseInt(product.rating);
+    var gray_length = 5 - parseInt(product.rating);
+    rating = '';
+    for (var k = 0; k < gold_length; k++) {
+        rating = rating + "<i><img src='images/star.png' class='starimg' aria-hidden='true'></i>";
+    }
+    for (var k = 0; k < gray_length; k++) {
+        rating = rating + "<i><img src='images/star-gray.png' class='starimg' aria-hidden='true'></i>";
+    }
+
+    let price = '$' + product.price;
+    document.getElementById('pdetails_image').innerHTML = image;
+    document.getElementById('pdetails_name').innerHTML = name;
+    document.getElementById('pdetails_desc').innerHTML = description;
+    document.getElementById('pdetails_rating').innerHTML = rating;
+    document.getElementById('pdetails_price').innerHTML = price;
+    document.getElementById('pdetails_category').innerHTML = product.category;
+    document.getElementById('pdetails_color').innerHTML = product.color;
+    document.getElementById('pdetails_type').innerHTML = product.type;
+    document.getElementById('pdetails_size').innerHTML = product.size;
+    document.getElementById('details').classList.remove("hidden");
+    document.getElementById('products_list').classList.add("hidden");
+    document.getElementById('pagination_bloack').classList.add("hidden");
+}
+
+function backToList() {
+    document.getElementById('products_list').classList.remove("hidden");
+    document.getElementById('details').classList.add("hidden");
+    document.getElementById('pagination_bloack').classList.remove("hidden");
+}
+
+/* Pagination */
+function prevPage() {
+    if (current_page > 1) {
+        current_page--;
+        var str = getProductList(main_product_list, current_page);
+        document.getElementById('products_list').innerHTML = str;
+    }
+}
+
+function nextPage() {
+    if (current_page < numPages(main_product_list)) {
+        current_page++;
+        var str = getProductList(main_product_list, current_page);
+        document.getElementById('products_list').innerHTML = str;
+    }
+}
+
+function numPages(objJson) {
+    return Math.ceil(objJson.length / records_per_page);
+}
